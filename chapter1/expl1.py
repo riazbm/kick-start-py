d92415fe80738b4fab84f118478f301323b39b9c
riziki <rizikimohamed968@gmail.com>
2024-03-26 15:40:46 +0300

    formatting strings using the % operator done

--- chapter1/expl1.py
+++ chapter1/expl1.py
@@ -16,3 +16,8 @@ mobileNumber = 789065756
 userHeight = 12.3
 userWeight = 60.7
 userName = 'Soso'.upper()
+
+brand = 'Apple'
+exchangeRate = 1.2345
+message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR' %(brand, 1299, exchangeRate)
+print(message)
