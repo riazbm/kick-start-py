fd0720def8f5a51588ff75e7229bf98bdc83f6b3
riziki <rizikimohamed968@gmail.com>
2024-03-26 16:00:41 +0300

    data types done from here

--- chapter1/expl1.py
+++ chapter1/expl1.py
@@ -22,4 +22,5 @@ exchangeRate = 1.2345
 message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR' %(brand, 1299, exchangeRate)
 print(message)
 message = 'The price of this {0:s} laptop is {1:d} USD and the exchange rate is {2:4.2f} USD to 1 EUR' .format('Apple', 1299, 1.2345)
+message = 'The price of this {} laptop is {} USD and the exchange rate is {} USD to 1 EUR' .format('Apple', 1299, 1.2345)
 
