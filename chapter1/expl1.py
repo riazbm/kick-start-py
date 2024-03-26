0b5785615e9918f238d62e9aaab3cc909672259e
riziki <rizikimohamed968@gmail.com>
2024-03-26 16:08:27 +0300

    use of input

--- chapter1/expl1.py
+++ chapter1/expl1.py
@@ -24,3 +24,4 @@ print(message)
 message = 'The price of this {0:s} laptop is {1:d} USD and the exchange rate is {2:4.2f} USD to 1 EUR' .format('Apple', 1299, 1.2345)
 message = 'The price of this {} laptop is {} USD and the exchange rate is {} USD to 1 EUR' .format('Apple', 1299, 1.2345)
 
+myName = input('enter your name:')
